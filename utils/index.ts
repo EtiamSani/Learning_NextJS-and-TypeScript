a0cf119440071ffe78c1b062962f0feaf7b3c2c9
EtiamSani <etiam.sani@laposte.net>
2023-07-31 14:16:55 +0200

    Add API

---
+++ utils/index.ts
@@ -0,0 +1,28 @@
+export async function fetchCars() {
+    const headers = {
+            'X-RapidAPI-Key': 'efc75c23a8mshedf8fffb60f35d0p1049b7jsn7bb1d18fd55c',
+            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers,}
+    );
+
+    const result = await response.json();
+
+    return result
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
+
