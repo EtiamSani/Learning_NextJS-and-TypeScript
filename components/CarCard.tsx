e8cf3763746ea27d11399e6b28b39c6624052cce
EtiamSani <etiam.sani@laposte.net>
2023-07-31 15:05:07 +0200

    modal

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -60,7 +60,7 @@ const CarCard = ({car}: CarCardProps) => {
                 containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
                 textStyles="text-white text-[14px] leading-[17px] font-bold"
                 rightIcon="/right-arrow.svg"
-                handleClick={()=> setIsOpen}/>
+                handleClick={()=> setIsOpen(true)}/>
             </div>
         </div>
         <CarDetails isOpen={isOpen} closeModal={()=> setIsOpen(false)} car={car}/>
